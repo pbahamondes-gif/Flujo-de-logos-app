b355358c2dccfa136bf3534a8bd88abf030a683a
pbahamondes-gif <pbahamondes@apprecio.com>
2026-04-02 16:33:31 -0300

    Update database password in db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 export const pool = mysql.createPool({
   host: "www.help-now.cl",
   user: "helpnowc_paulina",
-  password: "helpnowc_paulina",
+  password: "Paulina2026$",
   database: "cargaLogosCredenciales",
   waitForConnections: true,
   connectionLimit: 10,
